3c6c9b9479e6af7c683954d3e81bfbb54574090c
phí kiên <44530636+PhiKien@users.noreply.github.com>
2019-06-21 00:52:09 +0700

    music store lam lai

---
+++ StoreMusic/StoreMusic/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using StoreMusic.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StoreMusic.Controllers
+{
+    public class HomeController : Controller
+    {
+        StoreMusicContext storeDB = new StoreMusicContext();
+
+        public ActionResult Index()
+        {
+            var albums = GetTopSellingAlbums(5);
+            return View(albums);
+        }
+
+        private List<Album> GetTopSellingAlbums(int count)
+        {
+            return storeDB.Albums
+                    .OrderByDescending(a => a.OrderDetails.Count())
+                    .Take(count)
+                    .ToList();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+    }
+}
